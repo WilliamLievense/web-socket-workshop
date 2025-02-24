ecf23d0cecd5d40f89b47711a45614d3c00b2e27
Ms. Orret <71471052+msorret@users.noreply.github.com>
2025-02-24 09:48:15 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+http.listen(3000, function() {
+  console.log('Listening on port 3000');
+})
+
+// Handle a new connection
+io.on('connection', function(socket) {
+
+})
---
+++ public/script.js
@@ -0,0 +1,20 @@
+var socket = io()
+
+function setup() {
+  var canvas = createCanvas(400, 400)
+  canvas.parent('sketch-holder')
+
+  background("lightblue")
+}
+
+/****** DRAWING CODE  ******/
+
+function mouseDragged() {
+  
+  
+}
+
+function clearCanvas() {
+
+}
+
